d3ba3ce4ecfa6f00406b2af1b037c3e975c95af9
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2024-07-05 17:12:41 -0300

    page projects atualizada

--- pages/project.tsx
+++ pages/project.tsx
@@ -5,39 +5,42 @@ import { useState, useEffect } from "react";
 import { Projects } from "@/types/projects.interface";
 import MenuSelect from "@/app/components/MenuSelected";
 import { scrollToSection } from "@/utils/scrollSection";
+import wheatherApp from "../public/wheather_app.png";
+import todoApp from "../public/todo_app.png";
+import registroelogin from "../public/registroelogin.jpg";
 
 const Project = () => {
-  const [data, setData] = useState<Projects[]>([]);
-  const [originaldata, setOriginalData] = useState<Projects[]>([]);
+  // const [data, setData] = useState<Projects[]>([]);
+  // const [originaldata, setOriginalData] = useState<Projects[]>([]);
 
-  const techValue = (event: string) => {
-    techSelect(event);
-  };
+  // const techValue = (event: string) => {
+  //   techSelect(event);
+  // };
 
-  const techSelect = (e: string) => {
-    const result = originaldata.filter((project) => {
-      return project.tecnologias.includes(e);
-    });
+  // const techSelect = (e: string) => {
+  //   const result = originaldata.filter((project) => {
+  //     return project.tecnologias.includes(e);
+  //   });
 
-    console.log(result);
-    setData(result);
-  };
+  // console.log(result);
+  // setData(result);
+  // };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/api/projects");
-        const jsonData = await response.json();
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch("http://localhost:3000/api/projects");
+  //       const jsonData = await response.json();
 
-        setData(jsonData);
-        setOriginalData(jsonData);
-      } catch (error) {
-        console.error("Erro ao obter dados do MongoDB;", error);
-      }
-    };
+  //       setData(jsonData);
+  //       setOriginalData(jsonData);
+  //     } catch (error) {
+  //       console.error("Erro ao obter dados do MongoDB;", error);
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
   return (
     <>
@@ -45,16 +48,16 @@ const Project = () => {
       <Nav scrollToSection={scrollToSection}></Nav>
 
       <section className="min-w-screen min-h-screen bg-cinzaEscuro flex flex-col items-center">
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center mb-20">
           <span className="text-white text-5xl mt-24 flex justify-center">
             Portifólio
           </span>
-          <span className="text-cinzaClaro text-2xl mt-5 flex justify-center items-center">
+          {/* <span className="text-cinzaClaro text-2xl mt-5 flex justify-center items-center">
             Escolhe a tecnologia utilizada no projeto:
-          </span>
+          </span> */}
         </div>
 
-        <div className="flex justify-around mt-5 items-center w-[13.3rem] h-[3.3rem] bg-verde rounded-xl">
+        {/* <div className="flex justify-around mt-5 items-center w-[13.3rem] h-[3.3rem] bg-verde rounded-xl">
           <select
             name="Tech"
             id="Tech"
@@ -73,43 +76,98 @@ const Project = () => {
             <option value="REST Api">REST Api</option>
             <option value="SASS">SASS</option>
           </select>
-        </div>
+        </div> */}
 
-        <div className="flex flex-wrap justify-evenly my-10 w-[90%] sm:w-[70vw] items-center">
-          {data.map((item: Projects, index: number) => (
-            <div key={index} className="flex flex-col mb-20">
-              <div className="hover:scale-105 duration-500">
-                <div className="">
-                  <img
-                    src={item.imagem}
-                    alt="Imagem"
-                    className="rounded-xl w-[20rem] h-[19rem]"
-                  />
-                </div>
-              </div>
-
-              <div className="flex text-cinzaMedio justify-around text-xl mt-4 w-full flex-wrap">
-                {item.tecnologias.map((tech, i) => (
-                  <span key={i}>{tech}</span>
-                ))}
-              </div>
-
-              <div className="flex justify-evenly items-center mt-5">
-                <span className="text-white text-3xl mt-5 font-bold">
-                  {item.titulo}
-                </span>
-                <a href={item.urlGit} target="_blank">
-                  <Image
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg"
-                    alt="GitHub"
-                    width={40}
-                    height={40}
-                    className="filter brightness-0 invert cursor-pointer hover:scale-125 duration-500"
-                  ></Image>
-                </a>
-              </div>
+        <div className="flex flex-col md:flex-row justify-around w-[70%] min-h-80">
+          <div className="w-full md:w-[35%] transition-transform transform hover:scale-110 duration-500">
+            <Image
+              src={wheatherApp}
+              alt="Wheather App"
+              className="object-cover rounded-xl"
+            ></Image>
+          </div>
+          <div className="flex flex-col justify-center w-full md:w-[35%]">
+            <span className="text-verde text-xl">Wheather App</span>
+            <span className="text-white text-4xl mt-5">
+              Aplicativo de previsão de tempo
+            </span>
+            <span className="text-cinzaMedio mt-5 text-justify">
+              É um projeto desenvolvido para mobile, onde o usuário tem a opção
+              de pesquisar a temperatura da sua respectiva cidade ou qualquer
+              outra que seja.
+            </span>
+            <div className="mt-10 hover:scale-105 duration-500">
+              <a
+                className="bg-verde p-2 text-xl rounded-xl font-bold"
+                href="https://github.com/MarcosFernandes99/weather-app"
+                target="_blank"
+              >
+                Ver mais...
+              </a>
             </div>
-          ))}
+          </div>
+        </div>
+        <div className="flex flex-col-reverse md:flex-row justify-around w-[70%] min-h-80 mt-16">
+          <div className="flex flex-col justify-center w-full md:w-[35%]">
+            <span className="text-verde text-xl">Todo APP</span>
+            <span className="text-white text-4xl mt-5">
+              {" "}
+              Aplicativo de agendamento de tarefas
+            </span>
+            <span className="text-cinzaMedio mt-5 text-justify">
+              É um projeto onde o usuário tem a opção de inserir suas tarefas,
+              marcá-las como concluída ou se preferir exclui-las, como também
+              tem a opção de limpar a lista, onde todas as tarefas são
+              deletadas.
+            </span>
+            <div className="mt-10 transition-transform transform hover:scale-105 duration-500">
+              <a
+                href="https://github.com/MarcosFernandes99/todo-app?tab=readme-ov-file"
+                target="_blank"
+                className="bg-verde p-2 text-xl rounded-xl font-bold cursor-pointer"
+              >
+                Ver mais...
+              </a>
+            </div>
+          </div>
+          <div className="w-full md:w-[35%] transition-transform transform hover:scale-110 duration-500">
+            <Image
+              src={todoApp}
+              alt="Todo App"
+              className="rounded-xl object-cover"
+            ></Image>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row justify-around w-[70%] min-h-80 my-16">
+          <div className="w-full md:w-[35%] transition-transform transform hover:scale-110 duration-500">
+            <Image
+              src={registroelogin}
+              alt="Registro e Login"
+              className="object-contain rounded-xl"
+            ></Image>
+          </div>
+          <div className="flex flex-col justify-center w-full md:w-[35%]">
+            <span className="text-verde text-xl">Registro e Login</span>
+            <span className="text-white text-4xl mt-5">
+              Teste de Registro e Login
+            </span>
+            <span className="text-cinzaMedio mt-5 text-justify">
+              É um projeto onde o usuário faz o registro de uma conta, esses
+              dados serão armazenados no Local Storage. A seguir, o usuário é
+              direcionado a tela de login, onde ele precisa colocar o e-mail e
+              senha registrados anteriormente. Após análise dos dados, se
+              estiverem corretos, ele é direcionada ao Portal de notícias.
+            </span>
+            <div className="mt-10 hover:scale-105 duration-500">
+              <a
+                className="bg-verde p-2 text-xl rounded-xl font-bold"
+                href="https://github.com/MarcosFernandes99/registerLogin"
+                target="_blank"
+              >
+                Ver mais...
+              </a>
+            </div>
+          </div>
         </div>
       </section>
 
